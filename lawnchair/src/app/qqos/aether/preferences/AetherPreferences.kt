f4e80508b1c63354d25fc51f436f9c87d2f60aee
G-sahaab <gyawer575@gmail.com>
2026-08-08 01:16:56 +0530

    feat: add Aether preferences

---
+++ lawnchair/src/app/qqos/aether/preferences/AetherPreferences.kt
@@ -0,0 +1,13 @@
+package app.qqos.aether.preferences
+
+object AetherPreferences {
+
+    var enableBlur = true
+    var enableGlass = true
+    var enableAnimations = true
+    var enableDynamicTheme = true
+
+    var cornerRadius = 28
+    var blurRadius = 42
+    var animationSpeed = 250L
+}
