2546a42b121ae565da11b76406434bbe83bcb6c9
G-sahaab <gyawer575@gmail.com>
2026-08-08 01:06:22 +0530

    feat: add Aether core constants and theme

---
+++ lawnchair/src/app/qqos/aether/theme/AetherTheme.kt
@@ -0,0 +1,9 @@
+package app.qqos.aether.theme
+
+object AetherTheme {
+    const val PRIMARY = "#5EA8FF"
+    const val SECONDARY = "#7B61FF"
+    const val BACKGROUND = "#0B1220"
+    const val SURFACE = "#151C2E"
+    const val TEXT = "#FFFFFF"
+}
---
+++ lawnchair/src/app/qqos/aether/utils/Constants.kt
@@ -0,0 +1,10 @@
+package app.qqos.aether.utils
+
+object Constants {
+    const val OS_NAME = "QQOS"
+    const val UI_NAME = "Aether UI"
+    const val VERSION = "0.1 Alpha"
+    const val CODENAME = "Aether"
+    const val DEVICE = "veux"
+    const val AUTHOR = "QQ"
+}
